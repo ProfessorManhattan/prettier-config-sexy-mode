778ccf5a24194e74e58ddd7ed8e71a683f8ae652
Brian Zalewski <brian@megabyte.space>
2021-03-04 18:39:03 -0500

    feat(whole package): initial package
    Created a Prettier NPM configuration and included some other development features

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    bracketSpacing: true,
+    printWidth: 120,
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: false
+}
